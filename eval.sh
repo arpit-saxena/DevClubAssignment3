62e23e5fe427bf6ac09af4d6c5390a489fccd7ba
Arpit Saxena <arpit.saxena2000@yahoo.in>
2018-12-14 02:32:04 +0530

    Added script3 Not the best way, I reckon but just a makeshift way to do stuff.

---
+++ eval.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -z $1 ]
+then
+	echo "Usage: $0 filename"
+	exit -1
+fi
+
+file=$1
+
+ans=0
+while IFS='' read -r line || [[ -n "$line" ]]
+do
+	number=$(echo "$line" | sed -nE 's/([0-9]*).*/\1/p')
+	operator=$(echo "$line" | sed -nE 's/.*([-+\*\/]).*/\1/p')	
+	ans=$(( $ans $operator $number ))	
+done < "$1"
+
+echo $ans
