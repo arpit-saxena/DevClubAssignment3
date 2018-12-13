ef624016c584aef9e4c947a8ddea9c5f253f9fa9
Arpit Saxena <arpit.saxena2000@yahoo.in>
2018-12-13 23:09:22 +0530

    Tried some error handling with mkfifo. Doesn't work. Saving this stage to return to it later if needed

--- countFiles.sh
+++ countFiles.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-find $1 -maxdepth 1 -type f -name "*$2" | wc -l
-
+find $1 -maxdepth 1 -type f -name "*$2" 1> temp 2> /dev/null
+if [ $? -eq 0 ]
+then
+	wc -l <temp
+	if [ $? -ne 0 ]
+	then
+		exit -1
+	fi
+else
+	exit -1
+fi
