b1905eed66a2581fbf9b55593dfc1c694135c9b5
Arpit Saxena <arpit.saxena2000@yahoo.in>
2018-12-15 03:04:45 +0530

    Changed method to use cut for uname.sh; Improved input validation

--- uname.sh
+++ uname.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-if [ -z $2 ]
+if [ $# -ne 2 ]
 then
 	echo "Usage: $0 filename username"
+	exit 1
+elif [ ! -f $1 ]
+then
+	echo "$1 is not a file"
+	exit 1
+elif [ ! -r $1 ]
+then
+	echo "Unable to read $1"
 	exit 1
 fi
 
 #format: root:x:0:0:root:/root:/bin/bash
-regex='s/[a-zA-Z0-9]*:x:[0-9]*:[0-9]*:(.*):[a-zA-Z0-9_\/]*:[a-zA-Z0-9_\/]*.*/\1/p'
-sed -nE "/$2.*/p" < $1 | sed -nE "$regex"
+ans=$( cat $1 | grep -E "^$2:" | cut -d':' -f5 )
+
+if [ -z "$ans" ]
+then
+	echo "User not found"
+	exit 1
+else
+	echo $ans
+fi
 
