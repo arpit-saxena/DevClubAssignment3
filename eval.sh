6adedce62122071421068cf071a1eb774a37d2dd
Arpit Saxena <arpit.saxena2000@yahoo.in>
2018-12-15 03:02:59 +0530

    Changed method in eval.sh; Also improved input validation

--- eval.sh
+++ eval.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-if [ -z $1 ]
+if [ $# -ne 1 ]
 then
 	echo "Usage: $0 filename"
 	exit 1
+elif [ ! -f $1 ]
+then
+	echo "$1 is not a file"
+	exit 1
+elif [ ! -r $1 ]
+then
+	echo "Unable to read $1"u
+	exit 1
 fi
 
-file=$1
-
-ans=0
-while IFS='' read -r line || [[ -n "$line" ]]
-do
-	number=$(echo "$line" | sed -nE 's/([0-9]*).*/\1/p')
-	operator=$(echo "$line" | sed -nE 's/.*([-+\*\/]).*/\1/p')	
-	ans=$(( $ans $operator $number ))	
-done < "$1"
-unset IFS
-
-echo $ans
+awk 'BEGIN {print "var=0"} {print "var=var" $2 $1} END {print "var"}' "$1" | bc
